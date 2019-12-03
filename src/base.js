7f1077d5bd832547343c4b2a6e4677094493a434
Shane Hudson <shane@shanehudson.net>
2019-12-03 17:30:13 +0000

    Getting it ready for github

--- src/base.js
+++ src/base.js
@@ -1,15 +1,7 @@
 import Rebase from 're-base'
 import firebase from 'firebase'
 
-const firebaseApp = firebase.initializeApp({
-  apiKey: 'AIzaSyAZs-BfYZv3sLou5UnC3kuWx_uohLssQWg',
-  authDomain: 'buzzer-quiz.firebaseapp.com',
-  databaseURL: 'https://buzzer-quiz.firebaseio.com',
-  projectId: 'buzzer-quiz',
-  storageBucket: 'buzzer-quiz.appspot.com',
-  messagingSenderId: '179847450526',
-  appId: '1:179847450526:web:78320e90d1d94705edd08f'
-})
+const firebaseApp = {} // FIREBASE API CONFIG OBJECT
 
 const base = Rebase.createClass(firebaseApp.database())
 firebase.auth().signInAnonymously()
